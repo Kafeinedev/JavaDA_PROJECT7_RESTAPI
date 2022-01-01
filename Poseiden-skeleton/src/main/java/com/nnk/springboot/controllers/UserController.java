aab1ca1c9e126c9d2f977594ecfc836c21c18b61
Kafeinedev <kafeinedev@protonmail.com>
2022-01-01 17:38:07 +0100

    Linking user controller to service

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/UserController.java
@@ -1,9 +1,11 @@
 package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.User;
-import com.nnk.springboot.repositories.UserRepository;
+import com.nnk.springboot.services.UserService;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,61 +19,69 @@ import javax.validation.Valid;
 @Controller
 public class UserController {
 
+	Logger log = LogManager.getLogger("UserController");
+
 	@Autowired
-	private UserRepository userRepository;
+	private UserService userService;
 
 	@RequestMapping("/user/list")
 	public String home(Model model) {
-		model.addAttribute("users", userRepository.findAll());
+		log.info("RequestMapping @ /user/list");
+
+		model.addAttribute("userList", userService.findAllUsers());
+
 		return "user/list";
 	}
 
 	@GetMapping("/user/add")
 	public String addUser(User bid) {
+		log.info("GetMapping @ /user/add");
+
 		return "user/add";
 	}
 
 	@PostMapping("/user/validate")
 	public String validate(@Valid User user, BindingResult result, Model model) {
-		if (!result.hasErrors()) {
-			BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-			user.setPassword(encoder.encode(user.getPassword()));
-			userRepository.save(user);
-			model.addAttribute("users", userRepository.findAll());
-			return "redirect:/user/list";
+		log.info("PostMapping @ /user/validate");
+
+		if (result.hasErrors()) {
+			return "user/add";
 		}
-		return "user/add";
+		userService.createUser(user);
+
+		return "redirect:/user/list";
 	}
 
 	@GetMapping("/user/update/{id}")
 	public String showUpdateForm(@PathVariable("id") Integer id, Model model) {
-		User user = userRepository.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+		log.info("GetMapping @ /user/update/" + id);
+
+		User user = userService.findUserById(id);
 		user.setPassword("");
 		model.addAttribute("user", user);
+
 		return "user/update";
 	}
 
 	@PostMapping("/user/update/{id}")
 	public String updateUser(@PathVariable("id") Integer id, @Valid User user, BindingResult result, Model model) {
+		log.info("PostMapping @ /User/update/" + id);
+
 		if (result.hasErrors()) {
+			user.setId(id);
 			return "user/update";
 		}
+		userService.updateUser(id, user);
 
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		user.setPassword(encoder.encode(user.getPassword()));
-		user.setId(id);
-		userRepository.save(user);
-		model.addAttribute("users", userRepository.findAll());
 		return "redirect:/user/list";
 	}
 
 	@GetMapping("/user/delete/{id}")
 	public String deleteUser(@PathVariable("id") Integer id, Model model) {
-		User user = userRepository.findById(id)
-				.orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
-		userRepository.delete(user);
-		model.addAttribute("users", userRepository.findAll());
+		log.info("GetMapping @ /User/delete/" + id);
+
+		userService.deleteUserById(id);
+
 		return "redirect:/user/list";
 	}
 }
