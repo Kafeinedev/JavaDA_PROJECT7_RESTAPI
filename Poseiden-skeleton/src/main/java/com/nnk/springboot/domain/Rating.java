3ee4d1f5582257c72454ddfc8eaf43b8b159bf45
Kafeinedev <kafeinedev@protonmail.com>
2022-01-01 14:10:24 +0100

    Creating html form adding validation constraint

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/Rating.java
@@ -5,6 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -18,9 +23,22 @@ public class Rating {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+
+	@NotBlank(message = "Must not be blank")
+	@Size(max = 125, message = "Rating length cannot excede 125 characters")
 	private String moodysRating;
+
+	@NotBlank(message = "Must not be blank")
+	@Size(max = 125, message = "Rating length cannot excede 125 characters")
 	private String sandPRating;
+
+	@NotBlank(message = "Must not be blank")
+	@Size(max = 125, message = "Rating length cannot excede 125 characters")
 	private String fitchRating;
+
+	@NotNull(message = "Must not be null")
+	@Min(value = -127, message = "Order Number cannot be below -127")
+	@Max(value = 127, message = "Order Number cannot be above 127")
 	private Integer orderNumber;
 
 	public Rating(String moodysRating, String sandPRating, String fitchRating, Integer orderNumber) {
