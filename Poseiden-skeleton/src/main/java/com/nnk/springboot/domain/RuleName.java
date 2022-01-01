8f3b2701cfd693ea94b6167bc855b6f589d5a81c
Kafeinedev <kafeinedev@protonmail.com>
2022-01-01 16:37:06 +0100

    added form to html at rulename/* added constraint to ruleName

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/domain/RuleName.java
@@ -5,6 +5,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
@@ -18,11 +20,29 @@ public class RuleName {
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
+
+	@NotBlank(message = "Please add a name")
+	@Size(max = 125, message = "Maximum number of characters is 125")
 	private String name;
+
+	@NotBlank(message = "Please add a description")
+	@Size(max = 125, message = "Maximum number of characters is 125")
 	private String description;
+
+	@NotBlank(message = "Please add a json")
+	@Size(max = 125, message = "Maximum number of characters is 125")
 	private String json;
+
+	@NotBlank(message = "Please add a template")
+	@Size(max = 125, message = "Maximum number of characters is 125")
 	private String template;
+
+	@NotBlank(message = "Please add a sqlStr")
+	@Size(max = 125, message = "Maximum number of characters is 125")
 	private String sqlStr;
+
+	@NotBlank(message = "Please add a sqlPart")
+	@Size(max = 125, message = "Maximum number of characters is 125")
 	private String sqlPart;
 
 	public RuleName(String name, String description, String json, String template, String sqlStr, String sqlPart) {
